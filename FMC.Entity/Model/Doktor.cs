c020dae4d2b62617cb4dfc084e99475c1dbe1e27
Yusuf Palak <yusufplk28@gmail.com>
2022-03-11 23:05:15 +0300

    DoktorRepo,Service ve Controller Eklendi

---
+++ FMC.Entity/Model/Doktor.cs
@@ -0,0 +1,15 @@
+﻿using FMC.Entity.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMC.Entity.Model
+{
+    public class Doktor:BaseModel
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+    }
+}
---
+++ FMC.Service/Services/DoktorService.cs
@@ -0,0 +1,24 @@
+﻿using FMC.Entity.Model;
+using FMC.Repository.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMC.Service.Services
+{
+   public class DoktorService
+    {
+        private DoktorRepository doktorRepository = new DoktorRepository();
+
+        public List<Doktor> GetAll()
+        {
+            return doktorRepository.GetList(null);
+        }
+        public List<Doktor> GetAllByIsim(string isim)
+        {
+            return doktorRepository.GetList(x => x.Ad == isim);
+        }
+    }
+}
