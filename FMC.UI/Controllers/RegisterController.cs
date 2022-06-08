1f11b26f73e28e2a52ebc0eeb6918cc5199f185e
Yusuf Palak <yusufplk28@gmail.com>
2022-06-09 00:58:19 +0300

    Kayıt ve Giriş ekranı eklemek için RegisterController eklendi ve bu templatelere ait assets dosyaları eklendi.

---
+++ FMC.UI/Controllers/RegisterController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FMC.UI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
