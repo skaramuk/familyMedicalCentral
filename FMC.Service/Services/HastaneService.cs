4a9b5aa8b014f7e6a360c61d4f3db54145c9d514
Yusuf Palak <yusufplk28@gmail.com>
2022-06-12 15:46:14 +0300

    Service ve Controller eklendi.

---
+++ FMC.Service/Services/HastaneService.cs
@@ -0,0 +1,25 @@
+﻿using FMC.Entity.Model;
+using FMC.Repository.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMC.Service.Services
+{
+    public class HastaneService
+    {
+        private HastaneRepository hastaneRepository = new HastaneRepository();
+
+        public List<Hastane> GetAll()
+        {
+            return hastaneRepository.GetList();
+        }
+        public Hastane Add(Hastane hastane)
+        {
+            return hastaneRepository.Add(hastane);
+        }
+
+    }
+}
---
+++ FMC.UI/Controllers/HastaneController.cs
@@ -0,0 +1,31 @@
+﻿using FMC.Entity.Model;
+using FMC.Service.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FMC.UI.Controllers
+{
+    public class HastaneController : Controller
+    {
+        private HastaneService hastaneService = new HastaneService();
+        // GET: Hastane
+        public ActionResult Index()
+        {
+            return View(hastaneService.GetAll());
+        }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Hastane hastane)
+        {
+            hastaneService.Add(hastane);
+            return RedirectToAction("Index");
+        }
+    }
+}
