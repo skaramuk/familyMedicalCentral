5ac9e2cb5ca5a4ff6dbde00b2cbd195a09a5650d
Yusuf Palak <yusufplk28@gmail.com>
2022-06-12 15:07:33 +0300

    Migrations eklendi

---
+++ FMC.Repository/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace FMC.Repository.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FMC.Repository.Context.FmcDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(FMC.Repository.Context.FmcDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
