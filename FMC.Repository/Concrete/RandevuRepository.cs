5afd960845c0a809bee5fdcf5786ebe0ed98a99c
Yusuf Palak <yusufplk28@gmail.com>
2022-06-12 15:18:58 +0300

    Repository eklendi.

---
+++ FMC.Repository/Concrete/RandevuRepository.cs
@@ -0,0 +1,14 @@
+﻿using FMC.Entity.Model;
+using FMC.Repository.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMC.Repository.Concrete
+{
+    public class RandevuRepository:BaseRepository<Randevu>
+    {
+    }
+}
